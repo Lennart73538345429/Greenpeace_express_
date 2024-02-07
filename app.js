a2a1db532451077ac8c38557c6829647131874f2
Heureca <44292046+Lennart73538345429@users.noreply.github.com>
2024-02-07 22:53:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+//server.keepAliveTimeout = 120 * 1000;
+//server.headersTimeout = 120 * 1000;
 
 
 
